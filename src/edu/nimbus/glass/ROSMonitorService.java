092d9311c23e1977ca873c094e1968849860001e
Adam Taylor <aktaylor08@gmail.com>
2014-01-24 12:42:55 -0600

    Added licensing

--- src/edu/nimbus/glass/ROSMonitorService.java
+++ src/edu/nimbus/glass/ROSMonitorService.java
@@ -1,3 +1,18 @@
+/*
+copyright 2014 UNL Nimbus Lab 
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+  
+        http://www.apache.org/licenses/LICENSE-2.0
+  
+    Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 package edu.nimbus.glass;
 
 import android.app.PendingIntent;
@@ -18,7 +33,6 @@ import de.tavendo.autobahn.WebSocketException;
 /**
  * Service that will control the monitoring of a specific topic on ROS to monitor for warnings sent to the glass
  * when a warning is received this service will insert a live card into the timeline
- * @author ataylor
  *
  */
 public class ROSMonitorService extends Service {
